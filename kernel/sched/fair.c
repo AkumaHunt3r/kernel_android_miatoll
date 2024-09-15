489ffbccf86ae5dd46ece58e69b1cfbd2bd8f71e
shygosh <shygosh@proton.me>
2024-09-15 21:16:03 +0700

    sched/fair: Remove CAF active balance threshold
    Signed-off-by: shygosh <shygosh@proton.me>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -11187,7 +11187,6 @@ struct rq *find_busiest_queue(struct lb_env *env,
  * so long as it is large enough.
  */
 #define MAX_PINNED_INTERVAL	512
-#define NEED_ACTIVE_BALANCE_THRESHOLD 10
 
 static __always_inline int need_active_balance(struct lb_env *env)
 {
@@ -11537,8 +11536,7 @@ no_move:
 			}
 
 			/* We've kicked active balancing, force task migration. */
-			sd->nr_balance_failed = sd->cache_nice_tries +
-					NEED_ACTIVE_BALANCE_THRESHOLD - 1;
+			sd->nr_balance_failed = sd->cache_nice_tries+1;
 		}
 	} else
 		sd->nr_balance_failed = 0;
