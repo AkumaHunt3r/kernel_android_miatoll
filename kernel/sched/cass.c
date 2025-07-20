6ee62b816d8a1586b1b03cd761f193459d966859
AkumaHunt3r <AkumaHunt3r@protonmail.com>
2025-07-20 17:27:16 -0300

    sched: cass: Utilize idle latency and capacity headroom.
    In the same spirit of capacity headroom, implement a functionality to look for the CPU with the lowest latency to wake-up that also has the highest capacity headroom.

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -114,6 +114,11 @@ bool cass_cpu_better(const struct cass_cpu_cand *a,
 	if (cass_cmp(a->cap, b->cap))
 		goto done;
 
+	/* Prefer the CPU with lower idle exit latency and higher capacity headroom */
+	if (cass_cmp(b->exit_lat * b->cap / b->util,
+			 a->exit_lat * a->cap / a->util))
+		goto done;
+
 	/* Prefer the CPU with lower idle exit latency */
 	if (cass_cmp(b->exit_lat, a->exit_lat))
 		goto done;
