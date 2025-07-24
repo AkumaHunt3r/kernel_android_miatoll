cb5fae501c177b2c3f422d71a6fec18eeab6836a
AkumaHunt3r <AkumaHunt3r@protonmail.com>
2025-07-24 18:44:35 -0300

    sched: fair: Always allow migrating tasks.
    To allow us to leverage CASS more, allow tasks to be migrated regardless of fitting the CPU being checked for migration possibility. Potentially, CASS will be able to do a lot more work and improve performance and energy usage.

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -9420,10 +9420,12 @@ int can_migrate_task(struct task_struct *p, struct lb_env *env)
 		return 0;
 #endif
 
+#ifndef CONFIG_SCHED_CASS
 	/* Don't detach task if it doesn't fit on the destination */
 	if (env->flags & LBF_IGNORE_BIG_TASKS &&
 		!task_fits_max(p, env->dst_cpu))
 		return 0;
+#endif
 
 	if (task_running(env->src_rq, p)) {
 		schedstat_inc(p->se.statistics.nr_failed_migrations_running);
@@ -9537,8 +9539,10 @@ static int detach_tasks(struct lb_env *env)
 		if (!same_cluster(env->dst_cpu, env->src_cpu))
 			env->flags |= LBF_IGNORE_PREFERRED_CLUSTER_TASKS;
 
+#ifndef CONFIG_SCHED_CASS
 		if (cpu_capacity(env->dst_cpu) < cpu_capacity(env->src_cpu))
 			env->flags |= LBF_IGNORE_BIG_TASKS;
+#endif
 	}
 
 redo:
